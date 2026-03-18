e442391b14b6ced33cb4116c1f3376433f6b0131
KarimaChami <karimachami24@gmail.com>
2026-03-18 11:25:46 +0000

    feat: landing page initial setup

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -5,6 +5,8 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Apple } from "lucide-react";
 
+
+
 export default function CTA() {
   const ctaRef = useRef<HTMLDivElement>(null);
 
